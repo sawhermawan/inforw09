8c83c7b99a637bb69bd59f72c77625c5280aa6a9
sawhermawan <sayakeras@gmail.com>
2020-02-12 22:54:30 +0700

    laman web css edit

--- application/views/laman_web.php
+++ application/views/laman_web.php
@@ -7,16 +7,8 @@
     position: center;
     width: 800px;
     margin-left: 300px;
-    
-    
-
-    
-
   }
 </style>
-
-
-
 <style>
   .tren{
     text-shadow: 0px -2px 0px #ffffff,
@@ -25,16 +17,70 @@
     font-family: arial;
     background-color: transparent;
     font-style: border;
-    
+      }
+</style>
+<style>
+   #tren1  {
+    color: #f2f2f2; 
+    background: #ff4a4a;
+    font-size: 70px;
+    padding: 10px;
 
-  }
+      }
+</style>
+<style>
+   #tren2  {
+    color: #f2f2f2; 
+    background: #006cfa;
+    font-size: 50px;
+    font-family: arial;
+    padding: 7px;
+
+      }
+</style>
+<style>
+   #news1  {
+    color: #f2f2f2; 
+    background: #006cfa;
+    font-size: 70px;
+    padding: 10px;
+
+      }
+</style>
+
+<style>
+   #news2  {
+    color: #f2f2f2; 
+    background: #ff4a4a;
+    font-size: 50px;
+    font-family: arial;
+    padding: 7px;
+
+      }
 </style>
 
 <style>
    #tit{
 
-    background-color: #ffff00;
-    border: 10px inset white; 
+    background: linear-gradient(110deg, #ff8cfb 30%, #948cff 30%, #d1ff8c 80% );
+    border: 10px ridge white; 
+  }
+</style>
+
+<style>
+   #mek{
+
+      background-color: #eaeaea;
+     border: 10px ridge transparent;
+    background: linear-gradient(110deg, #bd6f6f 30%, #bd906f 30%, #d1e05a 80% );
+
+      }
+</style>
+
+<style>
+   #jdul{
+
+      color: green;
   }
 </style>
 <br>
@@ -46,7 +92,7 @@
         
     <div class="container animate-box" data-animate-effect="fadeIn">
         <div class="tren">
-            <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4" id="tren">Trending</div>
+            <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4" id="tren"><span id="tren1">Trending</span>&nbsp; <span id="tren2">Topic</span></div>
         </div>
         <div class="owl-carousel owl-theme js" id="slider1">
           <?php foreach ($m_home->result() as $laman): ?>
@@ -57,8 +103,8 @@
                       <img src="<?php echo base_url(); ?>assets/images/<?php echo $laman->picture; ?>" alt=""
                       class="fh5co_img_special_relative"/></div>
                     <div class="fh5co_latest_trading_img_position_absolute"></div>
-                    <div class="fh5co_latest_trading_img_position_absolute_1">
-                        <a href="<?php echo base_url('index.php/laman_web/view/'.$laman->berita_id)?>" class="text-white"> <?php echo $laman->judul_acara; ?> </a>
+                    <div class="fh5co_latest_trading_img_position_absolute_1" id="jdul">
+                        <a href="<?php echo base_url('index.php/laman_web/view/'.$laman->berita_id)?>" class="text-white" > <?php echo $laman->judul_acara; ?> </a>
                         <div class="fh5co_latest_trading_date_and_name_color"> <?php echo $laman->tanggal; ?></div>
                     </div>
                 </div>
@@ -110,21 +156,20 @@
 
 
 </div>
-        
-<br>
+<br><br>
+
+        <div class="tren">
+            <div class="fh5co_heading fh5co_heading_border_bottom py-2 mb-4" id="tren"><span id="news1">Informasi</span>&nbsp; <span id="news2">Kegiatan</span></div>
+        </div>
+
 
     <div class="row" >
-      <style>
-        #mek{
-          background: transparent;
-        }
-      </style>
       <?php foreach ($m_home->result() as $laman): ?>
       <div class="col-lg-4 col-sm-6 portfolio-item" class="fh5co_latest_trading_img_position_relative" >
-        <div class="card h-100" id="mek">
+        <div class="card h-100" id="mek" >
           
           <a ><img class="card-img-top" src="<?php echo base_url(); ?>assets/img/<?php echo $laman->picture; ?>"></a>
-          <div class="card-body">
+          <div class="card-body" >
             <h4 class="card-title">
               <p><?php echo $laman->judul_acara; ?></p>
             </h4>
@@ -135,12 +180,14 @@
                             <p><?php echo $laman->tanggal; ?></p>
             
           </div>
+
                 
         </div>
 
       </div>
 <?php endforeach; ?>
     </div>
+
 <!-- <div class="container-fluid pb-4 pt-5">
     <div class="container animate-box">
         <div>
--- application/views/static.php
+++ application/views/static.php
@@ -51,6 +51,10 @@
     <style>
     #pepek{
       background-color:#00e0e0;
+      padding-top: 1px;
+      padding-bottom: 3px;
+      margin-top:-6px;
+      position: center;
           }
 
   </style>
@@ -68,16 +72,16 @@
                         echo "| Pukul : ". $jam." "."";
                         $a = date ("H");
                         if (($a>=6) && ($a<=11)){
-                        echo ", Selamat Pagi Warga RW 09 Kutabumi Tangerang.. ";
+                        echo ", Selamat Pagi Warga RW 09 Pondok Indah Kutabumi Tangerang.. ";
                         }
                         else if(($a>11) && ($a<=15))
                         {
-                        echo ", Selamat Siang Warga RW 09 Kutabumi Tangerang.. " ;
+                        echo ", Selamat Siang Warga RW 09 Pondok Indah Kutabumi Tangerang.. " ;
                         }
                         else if (($a>15) && ($a<=18)){
-                        echo ", Selamat Sore Warga RW 09 Kutabumi Tangerang..";
+                        echo ", Selamat Sore Warga RW 09 Pondok Indah Kutabumi Tangerang..";
                         }
-                        else { echo ", <b> Selamat Malam Warga RW 09 Kutabumi Tangerang</b>";}
+                        else { echo ", <b> Selamat Malam Warga RW 09 Pondok Indah Kutabumi Tangerang</b>";}
                         ?> &nbsp;&nbsp;<i class="fa fa-smile-o fa-2x " aria-hidden="true"></i></a>
                 <div class="d-inline-block fh5co_trading_posotion_relative"><a href="#tren" class="treding_btn">Trending</a>
                     <div class="fh5co_treding_position_absolute"></div>
@@ -115,16 +119,16 @@
         </div>
     </div>
 </div>
-<div class="container-fluid bg-faded fh5co_padd_mediya padding_786" id="pepek">
+<div class="container-fluid bg-faded fh5co_padd_mediya padding_786" id="pepek" >
     <div class="container padding_786">
-        <nav class="navbar navbar-toggleable-md navbar-light ">
+        <nav class="navbar navbar-toggleable-md navbar-light " >
             <button class="navbar-toggler navbar-toggler-right mt-3" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation"><span class="fa fa-bars"></span></button>
             <a class="navbar-brand" href="#"><img src="images/logo.png" alt="img" class="mobile_logo_width"/></a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+                <ul class="navbar-nav mr-auto" >
+                    <li class="nav-item ">
                         <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item ">
@@ -132,24 +136,6 @@
                     </li>
                     <li class="nav-item ">
                         <a class="nav-link" href="single.html">Single <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuButton2" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">World <span class="sr-only">(current)</span></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink_1">
-                            <a class="dropdown-item" href="#">Action in</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdownMenuButton3" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">Community<span class="sr-only">(current)</span></a>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink_1">
-                            <a class="dropdown-item" href="#">Action in</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
                     </li>
                     <li class="nav-item ">
                         <a class="nav-link" href="Contact_us.html">Contact <span class="sr-only">(current)</span></a>
@@ -188,13 +174,27 @@
           </div>
         </div>
         <!-- Slide Three - Set the background image for this slide in the line below -->
- <!--        <div class="carousel-item" style="background-color: black;">
+        <div class="carousel-item" style="background-color: black;">
           <div class="carousel-caption d-none d-md-block">
-            <h3>Tujuan</h3>
+            <h1><?php $a = date ("H");
+                        if (($a>=6) && ($a<=11)){
+                        echo ", <b> Selamat Pagi, Salam Sejahtera..</b> ";
+                        }
+                        else if(($a>11) && ($a<=15))
+                        {
+                        echo ", <b> Selamat Malam, Selamat Beraktifitas..</b> " ;
+                        }
+                        else if (($a>15) && ($a<=18)){
+                        echo ", <b> Selamat Sore, Hati2x Berkendara Di Jalan Pulang ..</b>";
+                        }
+                        else { echo " <b> Selamat Malam, Selamat Beristirahat..</b>";}
+                        ?> 
+            </h1>
+            <br><br><br><br><br><br>
 
                           
           </div>
-        </div> -->
+        </div>
       </div>
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -211,7 +211,7 @@
   <!-- Footer -->
   <footer class="py-3 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; RW 09 Kutabumi Tangerang</p>
+      <p class="m-0 text-center text-white">Copyright &copy; RW 09 Kutabumi Tangerang 2020</p>
     </div>
     <!-- /.container -->
   </footer>
